87054c731185ab7dd4660360ecf823ae389c9655
Михаил Острецов <ostretsov_06@icloud.com>
2025-07-22 00:59:57 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -19,7 +19,7 @@ namespace Bot
     internal class Work 
     {
 
-        private static TelegramBotClient bot = new("7566820191:AAHdyTDFzjXeTASp_9ECfg-sax7fli9dfl4");
+        private static TelegramBotClient bot = new("");
         static public async Task Main()
         {
             try
